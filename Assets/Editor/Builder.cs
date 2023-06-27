22db7bc7f841ff3867f4d90df83680d21c008cd5
Rony <siamore9724@gmail.com>
2023-06-27 18:12:52 +0900

    builder retry

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityBuilder {
+    public static class Builder {
+        public static void Build() {
+            Console.WriteLine("Build is Called!!");
+        }
+    }
+}
