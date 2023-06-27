756dd10589aedbc264c7d1919ae175fe71158b7e
Rony <siamore9724@gmail.com>
2023-06-27 18:21:44 +0900

    예아

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace UnityBuilder {
     public static class Builder {
         public static void Build() {
             Console.WriteLine("Build is Called!!");
+            BuildPlayerWindow.RegisterBuildPlayerHandler(BuildPlayerHandler);
+        }
+        
+        public static void BuildPlayerHandler(BuildPlayerOptions options) {
+            BuildPlayerWindow.RegisterBuildPlayerHandler(BuildPlayerHandler);
+            options.locationPathName = "build/Android";
+            Console.WriteLine("yeah");
+            BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(options);
         }
     }
 }
