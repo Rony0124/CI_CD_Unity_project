e787739f1613b413a5d832387d6004904ce08e51
Rony <siamore9724@gmail.com>
2023-06-28 10:57:40 +0900

    custom Build Path 테스트

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,21 +1,55 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 namespace UnityBuilder {
     public static class Builder {
+        private static readonly string Eol = Environment.NewLine;
+
         public static void Build() {
             Console.WriteLine("Build is Called!!");
-            BuildPlayerWindow.RegisterBuildPlayerHandler(BuildPlayerHandler);
+
+            ParseCommandLineArguments(out Dictionary<string, string> validatedOptions);
+
+            var scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
+
+            string path = validatedOptions["customBuildPath"];
+
+            BuildPipeline.BuildPlayer(scenes, path, BuildTarget.Android, BuildOptions.None);
         }
-        
-        public static void BuildPlayerHandler(BuildPlayerOptions options) {
-            BuildPlayerWindow.RegisterBuildPlayerHandler(BuildPlayerHandler);
-           // options.locationPathName = "build/Android";
-            Console.WriteLine("yeah");
-            BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(options);
+
+        private static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments) {
+            providedArguments = new Dictionary<string, string>();
+            string[] args = Environment.GetCommandLineArgs();
+
+            Console.WriteLine(
+                $"{Eol}" +
+                $"###########################{Eol}" +
+                $"#    Parsing settings     #{Eol}" +
+                $"###########################{Eol}" +
+                $"{Eol}"
+            );
+
+            // Extract flags with optional values
+            for (int current = 0, next = 1; current < args.Length; current++, next++) {
+                // Parse flag
+                bool isFlag = args[current].StartsWith("-");
+                if (!isFlag) continue;
+                string flag = args[current].TrimStart('-');
+
+                // Parse optional value
+                bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                string value = flagHasValue ? args[next].TrimStart('-') : "";
+
+                string displayValue = "\"" + value + "\"";
+
+                // Assign
+                Console.WriteLine($"Found flag \"{flag}\" with value {displayValue}.");
+                providedArguments.Add(flag, value);
+            }
         }
     }
 }
